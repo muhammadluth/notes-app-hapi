90b4fc46c2c9e37d522c10a3c8b69a4358353916
Muhammad Luthfi <muhammadluthfi059@gmail.com>
2021-05-27 15:59:38 +0700

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
 const Hapi = require("@hapi/hapi");
 const routes = require("./routes");
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
-    host: "localhost",
+    port: process.env.PORT,
+    host:
+      process.env.NODE_ENV !== "production"
+        ? process.env.HOST_DEVELOPMENT
+        : process.env.HOST_PRODUCTION,
     routes: {
       cors: {
         origin: ["*"],
